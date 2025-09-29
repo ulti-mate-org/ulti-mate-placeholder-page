e3089645fd224e1192ce0c11b6d64965c72105ca
Simon9116 <simoneleveghi8@protonmail.com>
2025-09-29 22:07:49 +0200

    Improve carousel shift

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const heroCardsNodes = document.querySelectorAll('.hero-card');
 const heroCards = Array.from(heroCardsNodes);
 
-
-
 function positionCards() {
+    let offset = -1;
     heroCards.forEach((card, index) => {
-        card.style.transform = `translateX(${index * 135}%)`;
+        card.style.transform = `translateX(${(offset + index) * 135}%)`;
     });
 }
 
 positionCards();
 
+let startIndex = 0;
 setInterval(() => {
+    let transitionTmp = heroCards[0].style.transition;
+    heroCards[0].style.transition = 'none';
     heroCards.push(heroCards.shift());
+    heroCards[0].style.transition = transitionTmp;
+
     positionCards();
 }, 5000);
